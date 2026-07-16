c18199baf0f66c0442f93ea3a5511f5f8bd48964
yashithub <yashpradeep2005@gmail.com>
2026-07-16 21:48:52 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        bool found = false;
+        long long rate = 1;
+        while(found == false){
+            long long time = 0;
+            for(int i : piles){
+                // if (i%rate != 0) time += i/rate + 1;
+                // else time += i/rate;
+                time += ceil((double)i/rate);
+            }
+            if (time > h) rate++;
+            else {
+                found = true;
+                break;
+            }
+        }
+        return (int)rate;
+    }
+};
